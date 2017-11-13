7ba3b62d86dd551f352207b917deffad7f5cbcae
Jasna <popovic.jasna93@gmail.com>
2017-11-13 20:26:25 +0100

    RTL complete

--- hdl/fault_tolerant_fir.vhd
+++ hdl/fault_tolerant_fir.vhd
@@ -24,7 +24,7 @@ signal voter_out: std_logic_vector(data_w - 1 downto 0);
 
 component FIR
   generic (
-    order     : natural := 20;
+    order     : natural := 10;
     data_w    : natural := 24);
   port (
     clk          : in  std_logic;
